bd391e1aa05a3de63633b6b2916383514be500ed
andre <andrea1.fraschini@mail.polimi.it>
2023-12-14 19:18:36 +0100

    Added primitive wind visualization

--- python_scripts/wind_stuff/generate_wind_map.py
+++ python_scripts/wind_stuff/generate_wind_map.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 import shapely as shp
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 map = gpd.read_file('./python_scripts/REGIONE_LOMBARDIA/Comuni_correnti_poligonali.shp')
 map = map.to_crs({'init': 'epsg:4326'})
@@ -9,14 +10,29 @@ municipality_polygons = gpd.GeoDataFrame(map[["NOME_COM", "geometry"]], crs = ma
 
 wind_data = pd.read_csv("./python_scripts/wind_stuff/Aggregated_wind_data.csv")
 
-grid_points=[]
-for t in wind_data[["Longitude", "Latitude"]].itertuples():
-    grid_points.append(shp.Point(t[1], t[2]))
+max_speed = wind_data['WE_wind_speed_100m_mean'].max()
+max_length = 50
+arrows={'X':[], 'Y':[], 'U':[], 'V':[]}
+for i, data in wind_data.iterrows():
+    arrows['X'].append(data['Longitude'])
+    arrows['Y'].append(data['Latitude'])
+    
+    #from polar to cartesian
+    arg = data['WE_mode_wind_direction_100m']
+    #radius = data['WE_wind_speed_100m_mean']/max_speed*max_length
+    radius = data['WE_wind_speed_100m_mean']
 
-grid = gpd.GeoDataFrame(geometry=grid_points, crs = 'epsg:4326')
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    arrows['U'].append(np.cos(arg)*radius)
+    arrows['V'].append(np.sin(arg)*radius)
 
-municipality_polygons.plot(ax=ax, edgecolor='black', linewidth=0.5)
-grid.plot(ax=ax, color='red', markersize=5)
+#fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+
+ax = municipality_polygons.plot(edgecolor='black', color='white', linewidth=0.5)
+#plt.quiver(arrows['positions'], arrows['directions'])
+ax.quiver(arrows['X'],arrows['Y'],arrows['U'],arrows['V'], 
+          color='red',
+          headlength=10,
+          headaxislength=10
+)
 
 plt.show()
