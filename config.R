82b35591e8137d9cb8033c59719ed994010d8599
CamiloSinningUN <camilosinning.cs@gmail.com>
2024-02-09 00:45:02 +0100

    Set default config for model

--- config.R
+++ config.R
@@ -1,14 +1,14 @@
 #  Model parameters
-n_iterations <- 1000L # 10000L (Kim et al., 2020)
-n_trees <- 10L # 200L (Kim et al., 2020)
-warmup <- 100L # 1000L (Kim et al., 2020)
+n_iterations <- 2000L # 10000L (Kim et al., 2020)
+n_trees <- 200L # 200L (Kim et al., 2020)
+warmup <- 200L # 1000L (Kim et al., 2020)
 
 # Model filename
-model_filename <- "2016_Gen_to_Gen_model_30wind_10m_0"
+model_filename <- "2019_04_to_12_model_30wind_10m_0"
 
 # Range dates for data
-date_begin <- "2016-01-01"
-date_end <- "2016-01-05"
+date_begin <- "2019-04-01"
+date_end <- "2019-12-31"
 
 response_variable <- "AQ_pm25"
-covariates_of_interest <- c("Altitude", "LI_pigs", "LI_bovine")
+covariates_of_interest <- c("Altitude", "WE_temp_2m", "WE_wind_speed_10m_mean", "WE_wind_speed_10m_max", "WE_tot_precipitation", "WE_surface_pressure", "WE_solar_radiation", "WE_rh_min", "WE_rh_mean", "WE_rh_max", "WE_wind_speed_100m_mean", "WE_wind_speed_100m_max", "WE_blh_layer_max", "WE_blh_layer_min", "EM_nh3_livestock_mm", "EM_nh3_agr_soils", "EM_nh3_agr_waste_burn", "EM_nh3_sum", "EM_nox_traffic", "EM_nox_sum", "EM_so2_sum", "LI_pigs", "LI_bovine", "LA_hvi", "LA_lvi", "LA_land_use") # nolint
