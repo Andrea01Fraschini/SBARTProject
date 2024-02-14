997276e66cb875dbd4a94b82a8ebb163ac06feb5
CamiloSinningUN <camilosinning.cs@gmail.com>
2024-02-14 17:14:34 +0100

    New output to csv

--- output/helpers/output_to_csv.R
+++ output/helpers/output_to_csv.R
@@ -22,5 +22,23 @@ colnames(data_out) <- c("PM25_original", "PM25_predictions", covariates_of_inter
 covariates_inclusion <- model$covariates_selection_chain
 colnames(covariates_inclusion) <- covariates_of_interest
 
-write.csv(data_out, file = paste0("output/", model_filename, ".csv"), row.names = FALSE)
+# write.csv(data_out, file = paste0("output/", model_filename, ".csv"), row.names = FALSE)
 write.csv(covariates_inclusion, file = paste0("output/", model_filename, "_cov_inclusion", ".csv"), row.names = FALSE)
+
+
+# remove burnin
+burnin <- 0.5 * n_iterations
+burned_predictions <- model$y_prediction_history[, (burnin + 1):n_iterations]
+
+# apply thinning with a factor of 10
+thinning <- seq(1, burnin, by=10)
+thinned_predictions <- burned_predictions[, thinning]
+
+# compute point estimate
+point_estimates <- rowMeans(thinned_predictions)
+
+new_data <- data_out 
+missing_indexes <- which(is.na(new_data[, "PM25_original"]))
+new_data[missing_indexes, "PM25_predictions"] <- point_estimates
+
+write.csv(new_data, file = paste0("output/", model_filename, "_da.csv"), row.names = FALSE)
